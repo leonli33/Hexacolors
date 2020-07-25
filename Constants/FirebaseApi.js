95c1ffb990a500ac0c4d27aeb3e0349568956ebb
leonli33 <leonli3three@gmail.com>
2020-07-24 21:29:06 -0700

    Minor style fixes

--- Constants/FirebaseApi.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBNhvMO_n8O8TzCEFLC24KRGev9kH8Rh3k",
-  authDomain: "color-app-8e9c2.firebaseapp.com",
-  databaseURL: "https://color-app-8e9c2.firebaseio.com",
-  projectId: "color-app-8e9c2",
-  storageBucket: "color-app-8e9c2.appspot.com",
-  messagingSenderId: "702607550470",
-  appId: "1:702607550470:web:55db9e215f2964c8563b87",
-};
